4b282bf9b9c73fc4dddb7238a4d19fa0b1d9efab
Anjali Barge <bargeanjali650@gmail.com>
2025-04-30 16:35:32 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-def greet(name)
-    print(fHello, {name}! Welcome to Google Cloud.)
+def greet(name):
+    print(f"Hello, {name}! Welcome to Google Cloud.")
 
-if __name__ == __main__
-    user_name = input(Enter your name )
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
     greet(user_name)
